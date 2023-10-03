6a694541ac96e9329764bc20d8bcb984471798f4
Taira Terashima <taira.terashima@gmail.com>
2023-10-03 12:51:16 +0900

    Update

--- install.sh
+++ install.sh
@@ -49,9 +49,14 @@ preinstall_apt_packages() {
   runc \
   apt-transport-https \
   build-essential \
+  auditd \
+  nasm \
   pkg-config \
   libfuse-dev \
-  libssl-dev
+  libssl-dev \
+  qemu-system \
+  qemu-system-common \
+  qemu-utils
 }
 
 setup_apt_package_list() {
@@ -74,7 +79,7 @@ setup_apt_package_list() {
   #初回は必ず更新
   if [ ! -e update-time ]; then
     sudo apt update
-    sudo apt upgrade
+    sudo apt -y upgrade
     touch update-time
     date "+%s" > update-time
   else
@@ -83,7 +88,7 @@ setup_apt_package_list() {
     updated=$(cat update-time)
     if [ $(($now-$updated)) -gt 86400 ]; then
       sudo apt update
-      sudo apt upgrade
+      sudo apt -y upgrade
       date "+%s" > update-time
     fi
   fi
